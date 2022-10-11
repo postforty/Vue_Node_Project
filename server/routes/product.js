80e070a5bb4d518d19e93d61782b5e979832dc16
postforty <75767073+postforty@users.noreply.github.com>
2022-10-11 21:14:15 +0900

    [INITIAL] morgan, multer, cors 등

---
+++ server/routes/product.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const router = express.Router();
+const mysql = require("../mysql"); // mysql 폴더의 index.js를 가져옴
+
+router.get("/category", async (req, res) => {
+  const categoryList = await mysql.query("categoryList");
+  res.send(categoryList);
+});
+
+router.get("/category/:product_category_id", async (req, res) => {
+  const { product_category_id } = req.params;
+  const categoryList = await mysql.query("categoryDetail", product_category_id);
+  res.send(categoryList);
+});
+
+router.post("/category", async (req, res) => {
+  const result = await mysql.query("categoryInsert", req.body.param);
+  res.send(result);
+});
+
+router.put("/category/:product_category_id", async (req, res) => {
+  const { product_category_id } = req.params;
+  const result = await mysql.query("categoryUpdate", [
+    req.body.param,
+    product_category_id,
+  ]);
+  res.send(result);
+});
+
+router.delete("/category/:product_category_id", async (req, res) => {
+  const { product_category_id } = req.params;
+  const result = await mysql.query("categoryDelete", product_category_id);
+  res.send(result);
+});
+
+module.exports = router;
